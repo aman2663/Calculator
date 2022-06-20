6a1f5b0a58b0dee27adcc0a3b270d0bda926b601
aman2663 <104823207+aman2663@users.noreply.github.com>
2022-06-20 17:24:27 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,36 @@
+let displayxyz=document.getElementById('display');
+let buttonsxyz=document.getElementById('btn');
+console.log(buttonsxyz);
+function isOpxyz(opxyz){
+    if(opxyz=='+'||opxyz=='-'||opxyz=='*'||opxyz=='/')
+    return true;
+
+    return false;
+}
+function clickingxyz(clickedxyz){
+    if(displayxyz.innerText==""){
+        if(clickedxyz!='0')
+        displayxyz.innerText=displayxyz.innerText+clickedxyz;
+    }
+    else{
+        if(isOpxyz(clickedxyz)&&isOpxyz(displayxyz.innerText[displayxyz.innerText.length-1])){
+            displayxyz.innerText=displayxyz.innerText.slice(0,-1);
+        }
+        displayxyz.innerText=displayxyz.innerText+clickedxyz;
+    }
+}
+function evalxyz(){
+    try{
+      displayxyz.innerText = eval(displayxyz.innerText);
+    }
+    catch(err){
+      displayxyz.innerText = "Error";
+    }
+  }
+  function clearxyz(){
+    displayxyz.innerText = "";
+  }
+  
+  function backxyz(){
+    displayxyz.innerText = displayxyz.innerText.slice(0, -1);
+  }
